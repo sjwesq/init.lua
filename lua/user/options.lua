8b4db0991928e743a0caf97679d3b2bf3226aab0
sjwesq <sophie@sjwesq.dev>
2025-06-12 01:52:23 -0400

    Initial version

---
+++ lua/user/options.lua
@@ -0,0 +1,31 @@
+-- options.lua
+local opt = vim.opt
+
+opt.number = true
+vim.cmd [[colorscheme gruvbox]]
+opt.termguicolors = true
+
+opt.textwidth = 80
+vim.cmd [[let &colorcolumn=join(range(81,999),",")]]
+
+opt.list = true
+opt.listchars = "tab:▸ ,eol:¬,nbsp:␣,extends:›,precedes:‹"
+
+opt.ignorecase = true
+opt.smartcase = true
+
+vim.cmd("filetype plugin indent on")
+vim.wo.foldmethod = "syntax"
+--opt.foldexpr="nvim_treesitter#foldexpr()"
+vim.cmd("autocmd BufReadPost,FileReadPost * normal zR")
+opt.expandtab = true
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.softtabstop = 2
+opt.backspace = "indent,eol,start"
+opt.spell = true
+opt.spelllang = "en_us"
+opt.wildmenu = true
+opt.hidden = true
+opt.wildignore = "*.exe,*.dll,*.pdb,*.so"
+opt.completeopt = "menu,menuone,preview,noselect,noinsert"
---
+++ lua/user/plugins.lua
@@ -0,0 +1,239 @@
+-- plugins.lua
+-- This is the real monster
+
+-- Bootstrap Lazy.nvim if not installed
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+-- plugin list
+require("lazy").setup({
+  ------------------------------------------------------------------------------
+  --- Navigation/Misc
+  ------------------------------------------------------------------------------
+  "jghauser/mkdir.nvim",
+  "jlanzarotta/bufexplorer",
+  {"akinsho/toggleterm.nvim", version = "*", config = true},
+  "nvim-telescope/telescope.nvim",
+  {
+    "echasnovski/mini.nvim",
+    config = function()
+      require("mini.comment").setup()
+      require("mini.files").setup()
+    end
+  },
+  ------------------------------------------------------------------------------
+  --- LSP Setup
+  ------------------------------------------------------------------------------
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      vim.lsp.enable("lua_ls")
+    end
+  },
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup {
+        automatic_installation = true
+      }
+    end
+  },
+  "dense-analysis/ale",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require 'nvim-treesitter.install'.prefer_git = false
+      require 'nvim-treesitter.configs'.setup {
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = {
+          enable = true,
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<bs>",
+          },
+        },
+
+        -- Automatically install missing parsers when entering buffer
+        -- Recommendation: set to false if you don't have `tree-sitter` CLI
+        -- installed locally
+        auto_install = true
+      }
+    end
+  },
+  ------------------------------------------------------------------------------
+  --- Delimiter Plugins
+  ------------------------------------------------------------------------------
+  {
+    "kylechui/nvim-surround",
+    config = function()
+      require("nvim-surround").setup()
+    end
+  },
+  {
+    "windwp/nvim-autopairs",
+    config = function()
+      require('nvim-autopairs').setup()
+      -- spaces in autopairs
+      local npairs = require 'nvim-autopairs'
+      local Rule = require 'nvim-autopairs.rule'
+      local cond = require 'nvim-autopairs.conds'
+
+      local brackets = { { '(', ')' }, { '[', ']' }, { '{', '}' } }
+      npairs.add_rules {
+        -- Rule for a pair with left-side ' ' and right side ' '
+        Rule(' ', ' ')
+          -- Pair will only occur if the conditional function returns true
+          :with_pair(function(opts)
+            -- We are checking if we are inserting a space in (), [], or {}
+            local pair = opts.line:sub(opts.col - 1, opts.col)
+            return vim.tbl_contains({
+              brackets[1][1] .. brackets[1][2],
+              brackets[2][1] .. brackets[2][2],
+              brackets[3][1] .. brackets[3][2]
+            }, pair)
+          end)
+          :with_move(cond.none())
+          :with_cr(cond.none())
+          -- We only delete the pair of spaces when the cursor is as such: ( | )
+          :with_del(function(opts)
+            local col = vim.api.nvim_win_get_cursor(0)[2]
+            local context = opts.line:sub(col - 1, col + 2)
+            return vim.tbl_contains({
+              brackets[1][1] .. '  ' .. brackets[1][2],
+              brackets[2][1] .. '  ' .. brackets[2][2],
+              brackets[3][1] .. '  ' .. brackets[3][2]
+            }, context)
+          end)
+      }
+      -- For each pair of brackets we will add another rule
+      for _, bracket in pairs(brackets) do
+        npairs.add_rules {
+          -- Each of these rules is for a pair with left-side '( ' and
+          -- right-side ' )' for each bracket type
+          Rule(bracket[1] .. ' ', ' ' .. bracket[2])
+            :with_pair(cond.none())
+            :with_move(function(opts) return opts.char == bracket[2] end)
+            :with_del(cond.none())
+            :use_key(bracket[2])
+            -- Removes the trailing whitespace that can occur without this
+            :replace_map_cr(function(_) return '<C-c>2xi<CR><C-c>O' end)
+        }
+      end
+    end
+  },
+  ------------------------------------------------------------------------------
+  -- Auto-Completion
+  ------------------------------------------------------------------------------
+  {
+    "hrsh7th/nvim-cmp",
+    config = function()
+      local cmp = require 'cmp'
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+            {
+              name = 'cmdline',
+              option = {
+                ignore_cmds = {}
+              }
+            }
+          })
+      })
+      cmp.setup.cmdline('/', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+    end
+  },
+
+  {
+    "hrsh7th/cmp-cmdline",
+    config = function()
+    end
+  },
+  "hrsh7th/cmp-buffer",
+  "hrsh7th/cmp-nvim-lsp",
+  "SirVer/ultisnips",
+  ------------------------------------------------------------------------------
+  -- Eye Candy
+  ------------------------------------------------------------------------------
+  "ellisonleao/gruvbox.nvim",
+  'nvim-tree/nvim-web-devicons',
+  {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      require('lualine').setup {
+        icons_enabled = false
+      }
+    end
+  },
+  {
+    "akinsho/bufferline.nvim",
+    config = function()
+      require("bufferline").setup()
+    end
+  },
+  {
+    "folke/noice.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify"
+    },
+    config = function()
+      require("noice").setup({
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use
+          -- **Treesitter**
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+          },
+        },
+        popupmenu = {
+          backend = "cmp"
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+          bottom_search = false, -- use a classic bottom cmdline for search
+          command_palette = true, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false, -- add a border to hover docs + signature help
+        },
+      })
+
+    end
+  }
+})
+
