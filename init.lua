45926dc2a5f3809eb34bb9170cb8871bef124636
sjwesq <sophie@sjwesq.dev>
2025-06-12 21:05:15 -0400

    Restored user namespace -- better practice

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 -- init.lua
-require('plugins') -- All plugins and plugin configuration
-require('autocmd') -- All autocmds
-require('options') -- All options (vim.opt, etc)
-require('bindings') -- All keybinds and commands, incl. for plugins
+require('user.plugins') -- All plugins and plugin configuration
+require('user.autocmd') -- All autocmds
+require('user.options') -- All options (vim.opt, etc)
+require('user.bindings') -- All keybinds and commands, incl. for plugins
