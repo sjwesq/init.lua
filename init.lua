a50d1388145b8f85a35d79f9abf4567a7bf707ed
sjwesq <sjwesq@protonmail.com>
2025-08-03 12:47:03 -0400

    Fixed completion, ran StyLua

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 -- init.lua
-require('user.plugins') -- All plugins and plugin configuration
-require('user.autocmd') -- All autocmds
-require('user.options') -- All options (vim.opt, etc)
-require('user.bindings') -- All keybinds and commands, incl. for plugins
+require("user.plugins") -- All plugins and plugin configuration
+require("user.autocmd") -- All autocmds
+require("user.options") -- All options (vim.opt, etc)
+require("user.bindings") -- All keybinds and commands, incl. for plugins
