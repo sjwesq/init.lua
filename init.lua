b7358dc282f954568f5c49bc258978a6bafeaf30
sjwesq <sjwesq@protonmail.com>
2025-08-12 13:19:02 -0400

    Added override file

--- init.lua
+++ init.lua
@@ -3,3 +3,4 @@ require("user.plugins") -- All plugins and plugin configuration
 require("user.autocmd") -- All autocmds
 require("user.options") -- All options (vim.opt, etc)
 require("user.bindings") -- All keybinds and commands, incl. for plugins
+require("user.override") -- System-specific overrides
