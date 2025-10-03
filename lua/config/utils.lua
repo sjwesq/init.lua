1a66b106f884f9ddd232809fc515c329b73eeae4
sjwesq <sophie@sjwesq.dev>
2025-10-03 12:15:42 -0400

    fzf: refactored from vimscript to lua

--- lua/config/utils.lua
+++ lua/config/utils.lua
@@ -6,29 +6,40 @@ end
 
 -- Fuzzy Picker APIs -----------------------------------------------------------
 -- Using these makes keymaps easier to change :)
-local pck_layout_normal = "'window':{'width':0.8, 'height':0.8}, "
-local pck_layout_slim = "'window':{'width':0.333, 'height':0.8}, "
+local fzf = vim.fn["fzf#run"]
+local wrap = vim.fn["fzf#wrap"]
 
-local function cmd_close(str)
-  return "<Cmd>" .. str .. "<CR>"
-end
+local width_full = 0.8
+local height_full = 0.8
+local width_slim = 0.333333
 
 function utils.cmd_fuzzy_files()
-  local picker = "call fzf#run(fzf#wrap({"
-    .. pck_layout_normal
-    .. "'options':'--preview \"bat --color=always --style=plain {}\" -m', "
-    .. "}))"
-
-  vim.cmd(picker)
+  fzf(wrap({
+    window = { width = width_full, height = height_full },
+    options = '--preview "bat --color=always --style=plain {}" -m',
+  }))
+  -- vim.cmd(picker)
 end
-function utils.cmd_fuzzy_buffers()
-  local picker = "call fzf#run(fzf#wrap({"
-    .. pck_layout_slim
-    .. "'source': map(filter(range(1, bufnr('$')), 'buflisted(v:val)'), 'bufname(v:val)'), "
-    .. "'sink': 'buffer', "
-    .. "}))"
 
-  vim.cmd(picker)
+function utils.cmd_fuzzy_buffers()
+  local buf_list = {}
+  for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.api.nvim_buf_is_loaded(bufnr) and vim.bo[bufnr].buflisted then
+      local path_abs = vim.api.nvim_buf_get_name(bufnr)
+      local path_rel = vim.fn.fnamemodify(path_abs, ":.")
+      table.insert(
+        buf_list,
+        path_rel ~= "" and path_rel or bufnr .. " [No Name]"
+      )
+    end
+  end
+
+  fzf(wrap({
+    window = { width = width_slim, height = height_full },
+    options = "--accept-nth=1",
+    source = buf_list,
+    sink = "buffer",
+  }))
 end
 
 return utils
